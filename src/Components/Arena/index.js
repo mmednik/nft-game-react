926fc61004cd676c7bee52cced584242e242d7cf
mmednik <mmednik@gmail.com>
2022-03-03 15:55:23 -0300

    rendering boss & my monster(character)-nft

--- src/Components/Arena/index.js
+++ src/Components/Arena/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
-import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
-import SevenLegendaryMonsters from '../../utils/SevenLegendaryMonsters.json';
-import './Arena.css';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformBossData } from "../../constants";
+import SevenLegendaryMonsters from "../../utils/SevenLegendaryMonsters.json";
+import "./Arena.css";
 
 const Arena = ({ characterNFT }) => {
   const [gameContract, setGameContract] = useState(null);
@@ -24,44 +24,66 @@ const Arena = ({ characterNFT }) => {
 
       setGameContract(gameContract);
     } else {
-      console.log('Ethereum object not found');
+      console.log("Ethereum object not found");
     }
   }, []);
 
   useEffect(() => {
     const fetchBoss = async () => {
       const bossTxn = await gameContract.getBigBoss();
-      console.log('bossTxn:', bossTxn);
-      setBoss(transformCharacterData(bossTxn));
+      console.log("bossTxn:", bossTxn);
+      setBoss(transformBossData(bossTxn));
     };
-    if(gameContract) {
+    if (gameContract) {
       fetchBoss();
     }
   }, [gameContract]);
 
   return (
     <div className="arena-container">
-       {boss && (
-      <div className="boss-container">
-        <div className={`boss-content`}>
-          <h2>🔥 {boss.name} 🔥</h2>
-          <div className="image-content">
-            <img src={boss.imageURI} alt={`Boss ${boss.name}`} />
-            <div className="health-bar">
-              <progress value={boss.hp} max={boss.maxHp} />
-              <p>{`${boss.hp} / ${boss.maxHp} HP`}</p>
+      {boss && (
+        <div className="boss-container">
+          <div className={`boss-content`}>
+            <h2>🔥 {boss.name} 🔥</h2>
+            <div className="image-content">
+              <img src={boss.imageURI} alt={`Boss ${boss.name}`} />
+              <div className="health-bar">
+                <progress value={boss.hp} max={boss.maxHp} />
+                <p>{`${boss.hp} / ${boss.maxHp} HP`}</p>
+              </div>
+            </div>
+          </div>
+          <div className="attack-container">
+            <button className="cta-button" onClick={runAttackAction}>
+              {`💥 Attack ${boss.name}`}
+            </button>
+          </div>
+        </div>
+      )}
+
+      {characterNFT && (
+        <div className="players-container">
+          <div className="player-container">
+            <h2>Your Monster</h2>
+            <div className="player">
+              <div className="image-content">
+                <h2>{characterNFT.name}</h2>
+                <img
+                  src={characterNFT.imageURI}
+                  alt={`Character ${characterNFT.name}`}
+                />
+                <div className="health-bar">
+                  <progress value={characterNFT.hp} max={characterNFT.maxHp} />
+                  <p>{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</p>
+                </div>
+              </div>
+              <div className="stats">
+                <h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+              </div>
             </div>
           </div>
         </div>
-        <div className="attack-container">
-          <button className="cta-button" onClick={runAttackAction}>
-            {`💥 Attack ${boss.name}`}
-          </button>
-        </div>
-      </div>
-    )}
-
-      <p>CHARACTER NFT GOES HERE</p>
+      )}
     </div>
   );
 };
--- src/constants.js
+++ src/constants.js
@@ -13,4 +13,14 @@ const transformCharacterData = (characterData) => {
   };
 };
 
-export { CONTRACT_ADDRESS, transformCharacterData };
+const transformBossData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData, transformBossData };
